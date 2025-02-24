c4d3dde169f9b3f2719d1067291b8a69e8cae5cc
bilal <bilaltoor786786@gmail.com>
2025-02-24 15:47:31 +0500

    latest

--- app/Todo/Edit/[id]/page.jsx
+++ app/Todo/Edit/[id]/page.jsx
@@ -49,24 +49,7 @@ export default function EditTodo() {
       <ToastContainer />
       <div className="max-w-3xl mx-auto">
         <h1 className="text-3xl font-bold text-center mb-8">Edit Todo</h1>
-        <form className="relative isolate mt-8 flex items-center pr-1">
-      <label className="sr-only">
-        Email address
-      </label>
-      <input
-        required
-        type="email"
-        autoComplete="email"
-        name="email"
-        placeholder="Email address"
-        className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
-      />
-      <Button type="submit" arrow>
-        Get updates
-      </Button>
-      <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
-      <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" />
-    </form>
+  x
         {/* Edit Todo Form */}
 
         <div className="bg-white p-6 rounded-lg shadow-md mb-8">
