11ac1d49a22c891ec9509cc7629a8411fd73a855
Régis Daum <rdaum@john-steel.com>
2025-04-24 18:37:12 +0200

    UI image choice + edit mode fixed button

--- components/ShopList.jsx
+++ components/ShopList.jsx
@@ -12,12 +12,6 @@ const ShopList = ({ pageType }) => {
 	const { categories } = useCategories();
 	const [editMode, setEditMode] = useState(false);
 
-	const listingProducts =
-		pageType === "shoppingList"
-			? shops.filter((shop) => shop.tobuy && !shop.incart)
-			: shops;
-	const incartProducts = shops.filter((shop) => shop.tobuy && shop.incart);
-
 	return (
 		<>
 			<section className="shoplist mt-8">
